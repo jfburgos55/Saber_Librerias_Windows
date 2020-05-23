698a2bcff6369bb256d3b46becfb654d13244a8a
John Burgos <jfburgos55@misena.edu.co>
2020-05-22 22:21:45 -0500

    Aplicación para conocer las librerias.

---
+++ SaberLibreriasWindows/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        For Each Archivo As String In My.Computer.FileSystem.GetFiles(
+            "C:\Windows\System32",
+            FileIO.SearchOption.SearchTopLevelOnly,
+            "*.dll")
+            ListBox1.Items.Add(Archivo)
+        Next
+        Label1.Text = ListBox1.Items.Count
+
+    End Sub
+End Class
